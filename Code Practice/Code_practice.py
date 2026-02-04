200a8d10d77c89c7124c92174cf8a5b23aa260b1
Little-Wolfbyte <klobato86@gmail.com>
2026-02-04 19:50:56 +0000

    Improved file to use a summation formula

--- Code Practice/Code_practice.py
+++ Code Practice/Code_practice.py
@@ -10,14 +10,19 @@ class Calculator:
     def getData(self):
         print("I am now executing a method in class")
 
+    def Summation(self):
+        return self.firstnumber + self.secondnumber + Calculator.num
+
+
 #On the left is the variable. The vairable  is assigned to the object, which is after the equals sign
 #By calling the object with Calculator() we call the testing the constructor print
-Object = Calculator()
+Object = Calculator(2,3)
 
 #To call the method within the class, you now use the object followed by a dot, and then call the method
 #The object is case sensitive
 
 Object.getData()
+print(Object.Summation())
 
 #I can also call the variable within the Class too. See below:
 print(Object.num)
