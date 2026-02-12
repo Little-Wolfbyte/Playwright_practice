84d30a9765c789f73df05d39659ad4074efcbfe7
Little-Wolfbyte <klobato86@gmail.com>
2026-02-12 16:13:39 +0000

    corrected issues with code

--- PyTest Practice/Yields_Asserts_error.py
+++ PyTest Practice/Yields_Asserts_error.py
@@ -6,14 +6,13 @@ def pass_value():
 
 @pytest.fixture(scope="function")
 def yields():
-    return 43
+    print("true")
     yield #pause everything after yields
     print("close browser and cookies") #run after completing everything in def test
 
 def test(pass_value, yields):
     print("first test")
     assert pass_value == 42
-    assert yields == 42
 
 #What's Happening
 #When Python sees return 43, it immediately exits the function and sends back the value 43
