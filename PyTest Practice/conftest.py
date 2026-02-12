9486daf2a9765a10b27a5e9951942c77aab133bf
Little-Wolfbyte <klobato86@gmail.com>
2026-02-12 13:56:55 +0000

    Notes on conftest

---
+++ PyTest Practice/conftest.py
@@ -0,0 +1,4 @@
+# this file is used to house all py fixtures that will be used
+#repeatedly across different .py files.
+#when you run your test on the other .py files and
+#referrence a fixture in here, the test will pick it up
